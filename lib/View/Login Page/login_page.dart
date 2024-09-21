c508875ed9da32fda49a7589f65c3f588d050290
asna <asnashergeel@gmail.com>
2024-09-21 19:52:04 +0500

    Added const modifier

--- lib/View/Login Page/login_page.dart
+++ lib/View/Login Page/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tenth_fifth_pays/View/Terms&Condition%20License%20Page/terms_&_conditions_page.dart';
 import 'package:tenth_fifth_pays/View/Utils/app_logo.dart';
@@ -9,12 +8,14 @@ import '../../Controller/Login Page/login_page_controller.dart';
 
 
 class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  LoginPageController _loginPageController= Get.put(LoginPageController());
+  final LoginPageController _loginPageController= Get.put(LoginPageController());
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +78,7 @@ class _LoginPageState extends State<LoginPage> {
                           style: ElevatedButton.styleFrom(
                               backgroundColor: AppPrimaryColor.primaryColor
                           ),
-                          child: Text('Login',style: TextStyle(fontFamily: 'Arial',fontSize: 22,color: Colors.white,fontWeight: FontWeight.w600),),
+                          child: const Text('Login',style: TextStyle(fontFamily: 'Arial',fontSize: 22,color: Colors.white,fontWeight: FontWeight.w600),),
                           onPressed: (){
                              Get.to(TermsAndConditionsPage());
                           },
@@ -89,7 +90,7 @@ class _LoginPageState extends State<LoginPage> {
                     padding:  EdgeInsets.only(
                       top: height*0.05,
                     ),
-                    child: Center(child: Text('Don\'t have an account?',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
+                    child: const Center(child: Text('Don\'t have an account?',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
                   ),
                   Center(
                     child: TextButton(
@@ -103,14 +104,14 @@ class _LoginPageState extends State<LoginPage> {
                     padding:  EdgeInsets.only(
                       top: height*0.03,
                     ),
-                    child: Center(child: Text('Check below to accept the',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
+                    child: const Center(child: Text('Check below to accept the',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
                   ),
                   Padding(
                     padding:  EdgeInsets.only(
                       right: width*0.07,
                       left: width*0.07
                     ),
-                    child: Center(child: Text('Fifth Tenth Pays Terms & Conditions\n               and Privacy Policy',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
+                    child: const Center(child: Text('Fifth Tenth Pays Terms & Conditions\n               and Privacy Policy',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500))),
                   ),
                   Padding(
                     padding:  EdgeInsets.only(
@@ -131,7 +132,7 @@ class _LoginPageState extends State<LoginPage> {
                             checkColor: Colors.white,
                           )),
                         ),
-                        Flexible(
+                        const Flexible(
                             child: Text('I accept Tenth Fifth Pays Terms and Conditions',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500)))
                       ],
                     ),
@@ -155,7 +156,7 @@ class _LoginPageState extends State<LoginPage> {
                             checkColor: Colors.white, // Tick mark color
                           )),
                         ),
-                        Flexible(
+                        const Flexible(
                             child: Text('I accept Tenth Fifth Pays Privacy Policy',style: TextStyle(fontFamily: 'Arial',fontSize: 18,fontWeight: FontWeight.w500)))
                       ],
                     ),
