adc77dca92e99d81bffd5e84ee7306403e3f20e4
asna <asnashergeel@gmail.com>
2024-09-26 23:58:44 +0500

    commit

--- lib/View/User Guide/user_guide_page.dart
+++ lib/View/User Guide/user_guide_page.dart
@@ -8,15 +8,22 @@ class UserGuidePage extends StatelessWidget{
     return const Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(4),
-          child: Column(
-            children: [
-              Text("User Guide", style: TextStyle(fontFamily: 'Arial', fontWeight: FontWeight.bold, fontSize: 24),),
-              Text("1.) Setup/Edit Profile \nUser Information \nPayment Method \n2.) Account Settings \n3.) Contacts"
-                  "\nAdding Contacts \nMaking Favorites \nImages \n4.) Home Screen \nFunding Account \nSelect Recipients"
-                  "\nMaking Payments \n5.) Notifications \nRecipients \nFor Senders \n6.) Other Information \nTransaction History"
-                  "\nFAQs \nRefer a Friend", style: TextStyle(fontFamily: 'Arial', fontSize: 18),),
-            ],
+          padding: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("\nUser Guide\n", style: TextStyle(fontFamily: 'Arial', fontWeight: FontWeight.bold, fontSize: 24),),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text("1.) Setup/Edit Profile \nUser Information \nPayment Method \n\n2.) Account Settings \n\n3.) Contacts"
+                      "\nAdding Contacts \nMaking Favorites \nImages \n\n4.) Home Screen \nFunding Account \nSelect Recipients"
+                      "\nMaking Payments \n\n5.) Notifications \nRecipients \nFor Senders \n\n6.) Other Information \nTransaction History"
+                      "\nFAQs \nRefer a Friend", style: TextStyle(fontFamily: 'Arial', fontSize: 18),),
+                ),
+              ],
+            ),
           ),
         ),
       ),
