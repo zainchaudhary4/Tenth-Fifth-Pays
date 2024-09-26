23f8948f28df2d2fc02345a958c1e5d9ad6c448b
asna <asnashergeel@gmail.com>
2024-09-26 23:21:08 +0500

    Request Support Page

---
+++ lib/Controller/Request Support/request_support_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class RequestSupportController extends GetxController{
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
+
+}
---
+++ lib/View/Request Support/request_support_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:tenth_fifth_pays/Controller/Request%20Support/request_support_controller.dart';
+import '../Utils/colors.dart';
+import '../Widgets/textfields.dart';
+
+class RequestSupportPage extends StatelessWidget{
+  const RequestSupportPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final reqController = RequestSupportController();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Request Support", style: TextStyle(fontFamily: 'Arial', fontSize: 24, fontWeight: FontWeight.bold)),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:  EdgeInsets.only(
+                        left: width*0.04,
+                        top: height*0.03
+                    ),
+                    child: CustomTextField(
+                      label: 'Name',
+                      controller: reqController.nameController,
+                      textInputType: TextInputType.text,
+                    ),
+                  ),
+                  Padding(
+                    padding:  EdgeInsets.only(
+                      top: height*0.02,
+                      left: width*0.04,
+                    ),
+                    child: CustomTextField(
+                      label: 'Email',
+                      controller: reqController.emailController,
+                      textInputType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Padding(
+                    padding:  EdgeInsets.only(
+                      top: height*0.01,
+                      left: width*0.05,
+                    ),
+                    child: CustomTextField(
+                      label: 'Phone',
+                      controller: reqController.phoneController,
+                      textInputType: TextInputType.phone,
+                    ),
+                  ),
+                  Padding(
+                    padding:  EdgeInsets.only(
+                      top: height*0.01,
+                      left: width*0.05,
+                    ),
+                    child: CustomTextField(
+                      label: 'Message',
+                      controller: reqController.messageController,
+                      textInputType: TextInputType.text,
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Padding(
+                  padding:  EdgeInsets.only(
+                      top: height*0.02
+                  ),
+                  child: SizedBox(
+                    height: height*0.06,
+                    width: width*0.77,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: AppPrimaryColor.primaryColor
+                      ),
+                      child: const Text('Submit',style: TextStyle(fontFamily: 'Arial',fontSize: 22,color: Colors.white,fontWeight: FontWeight.w600),),
+                      onPressed: (){},
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
